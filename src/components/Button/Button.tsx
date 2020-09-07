d679d472bf2240190c26c3482260851c0cc75e88
Grzegorz <gkubiak92@gmail.com>
2020-09-07 22:27:17 +0200

    Added some styles to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,6 +4,9 @@ const Button = styled.button`
   background: transparent;
   border-radius: 4px;
   border: 4px solid palevioletred;
+  padding: 4px 8px;
+  font-size: 16px;
+  font-weight: 600;
   color: palevioletred;
   :hover {
     cursor: pointer;
