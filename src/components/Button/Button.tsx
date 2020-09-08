d2cbcda0e01eb59f73331eedda79310da7e379db
Grzegorz <gkubiak92@gmail.com>
2020-09-08 21:16:36 +0200

    Added icon prop to Button component, added tabler-icons to project, Styled login page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,28 @@
+import React from 'react';
 import styled from 'styled-components';
+import { ButtonProps } from './types';
 
-const Button = styled.button`
+const StyledButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: transparent;
   border-radius: 4px;
-  border: 4px solid palevioletred;
+  border: 4px solid black;
   padding: 4px 8px;
   font-size: 16px;
   font-weight: 600;
-  color: palevioletred;
+  color: black;
   :hover {
     cursor: pointer;
   }
 `;
+
+const Button: React.FC<ButtonProps> = ({ children, icon }) => (
+  <StyledButton>
+    {icon ? icon : null}
+    {children}
+  </StyledButton>
+);
 
 export default Button;
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Styled } from 'components/Styled';
 import styled from 'styled-components';
+import { BrandGoogle } from 'tabler-icons-react';
 
 const StyledLogin = styled.section`
   width: 100%;
@@ -8,13 +9,18 @@ const StyledLogin = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+  header {
+    margin-bottom: 48px;
+  }
 `;
+
+const googleIcon = <BrandGoogle size={24} color='black' />;
 
 const LoginPage: React.FC = () => (
   <StyledLogin>
     <header>Login</header>
-    <input type='text' />
-    <Styled.Button>Login</Styled.Button>
+    <Styled.Button icon={googleIcon}>SignIn with Google</Styled.Button>
   </StyledLogin>
 );
 
